ad92b4e665d9927948398ed07c753b1b5fcfd93e
Ramsey Feng <ramsey.feng@boehringer-ingelheim.com>
2021-02-01 16:10:31 +0800

    Initial implementation

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+    redVal = 0;
+    greenVal = 0;
+    blueVal = 0;
 
-  constructor() {}
+    constructor() {
+    }
 
+    calculate(): any {
+        return {
+            'background-color': `rgb(${this.redVal}, ${this.greenVal}, ${this.blueVal})`
+        };
+    }
 }
